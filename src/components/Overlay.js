923dbb98e3e988bdf0c797283b6cc9fa5593c01d
Debadutta Panda <debadebaduttapanda.7@gmail.com>
2022-08-22 23:08:35 +0530

    final code commit

--- src/components/Overlay.js
+++
@@ -1,9 +0,0 @@
-import ReactDOM from "react-dom";
-const Overlay=(props)=>{
-    return <>{ReactDOM.createPortal(
-        <div className="fixed w-full h-full bg-black opacity-0 ">{props.children}</div>,
-        document.querySelector("#overlay")
-    )}
-    </>
-};
-export default Overlay;
